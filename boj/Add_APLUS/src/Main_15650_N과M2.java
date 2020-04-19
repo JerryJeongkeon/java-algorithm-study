6b3b1214957d1cd0e08b0d10c4f438f473344c35
jerryJK <vonjobi2@gmail.com>
2020-04-20 01:58:32 +0900

    N&M 2-3

--- boj/Add_APLUS/src/Main_15650_N과M2.java
+++ boj/Add_APLUS/src/Main_15650_N과M2.java
@@ -3,35 +3,38 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main_15649_N과M1 {
-	static int[] arr;
+public class Main_15650_N과M2 {
 	static int N, M;
+	static StringTokenizer st;
 	static boolean[] used;
 	static StringBuilder sb;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		st = new StringTokenizer(br.readLine(), " ");
+		sb = new StringBuilder();
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 
 		used = new boolean[N + 1];
-		sb = new StringBuilder();
-		
-		solve(0, "");
+		solve(1, 0, "");
+
 		System.out.println(sb.toString());
 	}
-	
-	static void solve(int cnt, String str) {
+
+	static void solve(int idx, int cnt, String str) {
 		if (cnt == M) {
 			sb.append(str.trim() + "\n");
 			return;
 		}
 
-		for (int i = 1; i <= N; i++) {
+		if (idx > N)
+			return;
+
+		for (int i = idx; i <= N; i++) {
 			if (!used[i]) {
 				used[i] = true;
-				solve(cnt + 1, str + i + " ");
+				solve(i, cnt + 1, str + i + " ");
 				used[i] = false;
 			}
 		}
--- boj/Add_APLUS/src/Main_15651_N과M3.java
+++ boj/Add_APLUS/src/Main_15651_N과M3.java
@@ -3,36 +3,32 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main_15649_N과M1 {
-	static int[] arr;
+public class Main_15651_N과M3 {
 	static int N, M;
+	static StringTokenizer st;
 	static boolean[] used;
 	static StringBuilder sb;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		st = new StringTokenizer(br.readLine(), " ");
+		sb = new StringBuilder();
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 
 		used = new boolean[N + 1];
-		sb = new StringBuilder();
-		
 		solve(0, "");
+
 		System.out.println(sb.toString());
 	}
-	
+
 	static void solve(int cnt, String str) {
 		if (cnt == M) {
 			sb.append(str.trim() + "\n");
 			return;
-		}
-
-		for (int i = 1; i <= N; i++) {
-			if (!used[i]) {
-				used[i] = true;
+		} else {
+			for (int i = 1; i <= N; i++) {
 				solve(cnt + 1, str + i + " ");
-				used[i] = false;
 			}
 		}
 	}
