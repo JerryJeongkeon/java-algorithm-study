bcf225b5b1ace3eb49aceff5ae5a4a62bf0920a5
Mango <vonjobi2@gmail.com>
2020-06-24 21:56:30 +0900

    문자열압축 solving2

--- programmers/src/programmers_Lv2/문자열압축.java
+++ programmers/src/programmers_Lv2/문자열압축.java
@@ -11,39 +11,52 @@ public class 문자열압축 {
 		if (answer <= 2) {
 			System.out.println(answer);
 		} else {
-			for (int i = size / 2; i > 1; i--) {
-				System.out.println("i = " + i + " 시작");
+			for (int i = size / 2; i >= 1; i--) {
 				int min = check(s, i);
 				if (min < answer)
 					answer = min;
 			}
 		}
+		System.out.println("answer : " + answer);
 	}
 
 	// len 개씩 단어를 잘라서 압축시켜본다
 	static int check(String str, int len) {
 		int length = s.length();
-		int cnt = 0;
+		int cnt = len;
 		String split = "";
+		int sameCheck = 0;
 
-//		static String s = "aabbaccc";
-
+		System.out.println("len : " + len + "----");
 		for (int k = 0; k + len < length; k += len) {
-
+			System.out.println("cnt : " + cnt + "k : " + k);
 			// 비교할 n개씩 자른 단어 뽑기
 			String temp = str.substring(k, k + len);
-
-			System.out.println(k + 2 * len - 1);
-			if (k + (2 * len) - 1 < length && str.substring(k + len, k + 2 * len).equals(temp)) {
-				System.out.println(temp + "와 " + str.substring(k + len, k + 2 * len) + " 비교 !");
-				if (temp.equals(split)) {
-					
-				} else
+			if (temp.equals(split)) {
+				k += len;
+				sameCheck+=len+1;
+				System.out.println("k:"+ k +", " + sameCheck);
+				
+				System.out.println(sameCheck);
+				if (sameCheck >= 10)
 					cnt++;
+				else if (sameCheck >= 100)
+					cnt++;
+				continue;
+			} else
+				sameCheck = 0;
+
+			if (k + (2 * len) <= length && str.substring(k + len, k + 2 * len).equals(temp)) {
+				cnt += len + 1;
+				k += len;
+
 				split = temp;
+			} else {
+				cnt += len;
 			}
 		}
-
-		return length;
+		cnt += length % len;
+		System.out.println("len :  "+ len + " cnt : " + cnt);
+		return cnt;
 	}
 }
